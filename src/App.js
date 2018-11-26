3ca28639f74c7546ff17e3332bccacfac9abd66e
mkfreeman <michael.k.freeman@gmail.com>
2018-11-25 20:53:10 -0800

    Build project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,107 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Auth from './Auth';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import Tweet from './Tweet';
+import "./App.css";
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null,
+            tweets: {}
+        };
+    }
+
+    // Begin listening to authentication *and* database on Mount()
+    componentDidMount() {
+        // Listen to state authentication state change
+        firebase.auth().onAuthStateChanged((user) => {
+            // If there is a user, set the state of `user`
+            if (user) {
+                this.setState({
+                    user: user
+                });
+            } else {
+                this.setState({ user: null });
+            }
+        });
+
+        // Create a reference to the "tweets" reference on the database
+        this.tweetsRef = firebase.database().ref('tweets');
+
+        // When the "tweets" *value* changes, update the state appropriately (`tweets`)
+        this.tweetsRef.on('value', (snapshot) => {
+            let tweets = snapshot.val();
+            this.setState({ tweets: tweets });
+        });
+
+    }
+
+    // Method to push tweet to the firebase database
+    sendTweet() {
+        // Construct `tweet` object to push to firebase
+        // Include the user's displayName, the text, number of likes (0), and a timestamp
+        let tweet = {
+            user: firebase.auth().currentUser.displayName,
+            timestamp: firebase.database.ServerValue.TIMESTAMP,
+            text: this.state.tweetText,
+            likes: 0
+        };
+
+        // Push the tweet to the database
+        this.tweetsRef.push(tweet).catch((d) => console.log("error", d));
+    }
+
+    // Method for updating the likes on a tweet
+    updateLikes(tweetId) {
+        // Create a reference to the number of likes
+        let likes = this.tweetsRef.child(tweetId + '/likes')
+
+        // Issue a transaction on the number of likes to increase it by 1
+        likes.transaction((d) => d + 1);
+    }
+
+    render() {
+        // Sort the keys of the tweets in descending order (by timestamp)
+        let sortedKeys = Object.keys(this.state.tweets).sort((a, b) => {
+            return this.state.tweets[b].timestamp - this.state.tweets[a].timestamp;
+        });
+
+        return (
+            <div className="App">
+                {/* Only display the auth if there is *not* a user */
+                    !this.state.user && <Auth />}
+                {/* Only display the twitter content if there *is* a user */
+                    this.state.user &&
+                    <div className="container">
+                        Welcome, {this.state.user.displayName}!
+                        <div>
+                            {/* Create an input that holds (and updates) the state of tweetText*/}
+                            <input
+                                placeholder="Share something..."
+                                className="form-control"
+                                value={this.state.tweetText}
+                                onChange={(event) => this.setState({ tweetText: event.target.value })}
+                            />
+                            {/* Create a button that, on click, executes the sendTweet() method*/}
+                            <button className="btn btn-primary" onClick={() => this.sendTweet()}>Tweet!</button>
+                        </div>
+                        <div className="tweetContainer">
+                            {
+                                /* For each key in sortedKeys, return a <Tweet />
+                                    - Make sure to pass in the tweet info, id, and an update() method
+                                */
+                                sortedKeys
+                                    .map((d) => {
+                                        return <Tweet update={(d) => this.updateLikes(d)} id={d} key={d} info={this.state.tweets[d]}></Tweet>
+                                    })}
+                        </div>
+                    </div>
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Tweet.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Tweet.css';
+
+// Create a functional component to return the HTMl that looks like a tweet
+function Tweet(props) {
+    // Format date
+    let date = new Date(props.info.timestamp);
+    let formattedDate = date.toLocaleTimeString();
+
+    /* Return a div that contains the following information:
+        - A div that has the user name (in bold) and the formatted date (use the className "date")
+        - A div that has the text (use className "tweetText")
+        - A div that has the number of likes (with className "likes"). On click, this should do the update function
+    */
+
+    return (
+        <div className="tweet">
+            <div className="user"><strong>{props.info.user + " "}</strong><span className="date">{formattedDate}</span></div>
+            <div className="tweetText">{props.info.text}</div>
+            <div className="likes" onClick={() => props.update(props.id)}>Likes: {props.info.likes}</div>
+        </div>
+    )
+}
+
+export default Tweet;
