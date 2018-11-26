3a4ad6cc0a8b79527fff3e4f44205ca3936346eb
mkfreeman <michael.k.freeman@gmail.com>
2018-11-25 21:03:58 -0800

    Add initial text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         super(props);
         this.state = {
             user: null,
-            tweets: {}
+            tweets: {},
+            tweetText: ''
         };
     }
 
